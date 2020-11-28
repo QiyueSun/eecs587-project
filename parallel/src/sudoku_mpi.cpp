8e5d69f1afc5d0512ce82742a64e464dae4c7a2d
Sungo <jerrysun@umich.edu>
2020-11-27 23:05:33 -0500

    debug

--- parallel/src/sudoku_mpi.cpp
+++ parallel/src/sudoku_mpi.cpp
@@ -164,49 +164,43 @@ retry:
         else {
             goto bailout;
         }
+
+        MPI_Bcast(kMATRIX, SIZE * SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+
+        // twin
+        if (!change) {
+            if (comm_rank == 0) {
+                SDK_Mark_Vertical_Availables_Twins(kMATRIX, 0, SIZE);
+                cout << "SDK_Mark_Vertical_Availables_Twins" << endl;
+                int32_t tmp_arr[SIZE*SIZE];
+                MPI_Recv(tmp_arr, SIZE * SIZE, MPI_INT, 1, 0, MPI_COMM_WORLD, NULL);
+                bool change_1 = SDK_Apply(kMATRIX, tmp_arr);
+                MPI_Recv(tmp_arr, SIZE * SIZE, MPI_INT, 2, 0, MPI_COMM_WORLD, NULL);
+                bool change_2 = SDK_Apply(kMATRIX, tmp_arr);
+                change = change_1 || change_2;
+            }
+            else if (comm_rank == 1) {
+                SDK_Mark_Horizontal_Availables_Twins(kMATRIX, 0, SIZE);
+                MPI_Send(kMATRIX, SIZE * SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            }
+            else if (comm_rank == 2) {
+                SDK_Mark_Subbox_Availables_Twins(kMATRIX, 0, SIZE);
+                MPI_Send(kMATRIX, SIZE * SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            }
+            else {
+                goto bailout;
+            }
+
+            MPI_Bcast(kMATRIX, SIZE * SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+
+            if (!change) {
+                if (comm_rank == 0) {
+                    cout << "Need triple" << endl;
+                }
+                goto bailout;
+            }
+        }
     }
-
-    MPI_Bcast(kMATRIX, SIZE * SIZE, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // twin
-    int32_t old_kMATRIX[SIZE*SIZE];
-    for (int i=0; i<SIZE*SIZE; i++) {
-      old_kMATRIX[i] = kMATRIX[i];
-    }
-
-    if (!change) {
-        if (comm_rank == 0) {
-            SDK_Mark_Vertical_Availables_Twins(kMATRIX, 0, SIZE);
-            cout << "SDK_Mark_Vertical_Availables_Twins" << endl;
-            int32_t tmp_arr[SIZE*SIZE];
-            MPI_Recv(tmp_arr, SIZE * SIZE, MPI_INT, 1, 0, MPI_COMM_WORLD, NULL);
-            bool change_1 = SDK_Apply(kMATRIX, tmp_arr);
-            MPI_Recv(tmp_arr, SIZE * SIZE, MPI_INT, 2, 0, MPI_COMM_WORLD, NULL);
-            bool change_2 = SDK_Apply(kMATRIX, tmp_arr);
-            change = change_1 || change_2;
-        }
-        else if (comm_rank == 1) {
-            SDK_Mark_Horizontal_Availables_Twins(kMATRIX, 0, SIZE);
-            MPI_Send(kMATRIX, SIZE * SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD);
-        }
-        else if (comm_rank == 2) {
-            SDK_Mark_Subbox_Availables_Twins(kMATRIX, 0, SIZE);
-            MPI_Send(kMATRIX, SIZE * SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD);
-        }
-        else {
-            goto bailout;
-        }
-    }
-
-    MPI_Bcast(kMATRIX, SIZE * SIZE, MPI_INT, 0, MPI_COMM_WORLD);
-
-    if (!change) {
-        if (comm_rank == 0) {
-            cout << "Need triple" << endl;
-        }
-        goto bailout;
-    }
-
 
     if (SDK_Check_Breakdown(kMATRIX)) {
         if (is_master) {
